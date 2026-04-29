7a7323d5a379a02c207b274031be0d296de6a343
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-04-29 19:03:53 +0530

    Create the server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
