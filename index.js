975bd6288720fef885b133a1079d884ca1b97f79
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-02 17:08:26 +0530

    setup dotenv

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const app = express();
 
 app.use(express.json());
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
