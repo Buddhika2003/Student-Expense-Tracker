fe00a48d67f913de15e5ac51c9559ad0aead3088
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-11 16:01:34 +0530

    update budget into more clean

--- model/budget.js
+++ model/budget.js
@@ -2,21 +2,9 @@ import mongoose from "mongoose";
 
 const budgetSchema = new mongoose.Schema(
   {
-    month: {
-      type: Number,
-      required: true
-    },
-
-    year: {
-      type: Number,
-      required: true
-    },
-
-    amount: {
-      type: Number,
-      required: true
-    },
-
+    month: {type: Number,required: true},
+    year: {type: Number,required: true},
+    amount: {type: Number,required: true},
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
