4350f90b1cbe391310538aebe6a56b71d983a245
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-11 12:18:44 +0530

    create route to display all expenses

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createExpense } from "../controller/expenseController.js";
+import {createExpense,getExpenses} from "../controller/expenseController.js";
 
 const router = express.Router();
 
 router.post("/expenses", createExpense);
+router.get("/expenses", getExpenses);
 
 export default router;
