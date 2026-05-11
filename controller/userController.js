a6a10e649b2928860234adfd36971fb167925546
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-11 14:49:55 +0530

    login method create for user

--- controller/userController.js
+++ controller/userController.js
@@ -30,3 +30,36 @@ export const registerUser = async (req, res) => {
 };
 
 
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
