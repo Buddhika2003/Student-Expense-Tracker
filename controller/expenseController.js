9f2f47b3f235cb7a44eced028eabcf4beacdca2b
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-11 12:07:20 +0530

    add method to getExpenses

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -9,4 +9,11 @@ export const createExpense = async (req, res) => {
   }
 };
 
-
+export const getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find();
+    res.json(expenses);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
