c0c5679ac9f1b4960a4efcafb2d8650300d61308
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-11 15:58:24 +0530

    adding user to expense model
    to connect expense into user

--- model/expense.js
+++ model/expense.js
@@ -6,7 +6,11 @@ const expenseSchema = new mongoose.Schema(
     amount: { type: Number, required: true },
     category: { type: String, required:true },
     date: { type: Date, default: Date.now },
-    user: {type: mongoose.Schema.Types.ObjectId,ref: "User",required: true}
+    user: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  required: true
+}
   },
   { timestamps: true }
 );
